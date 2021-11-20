8be2ce9fd64b4abd415f4a6c3534131470b628d6
gxz2018 <94280624+gxz2018@users.noreply.github.com>
2021-11-20 19:45:55 +0800

    Update shadowsocks-all.sh

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -38,7 +38,7 @@ cur_dir=$( pwd )
 software=(Shadowsocks-Python ShadowsocksR Shadowsocks-Go Shadowsocks-libev)
 
 libsodium_file='libsodium-1.0.18'
-libsodium_url='https://github.com/jedisct1/libsodium/releases/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz'
+libsodium_url='https://github.com/gxz2018/ssr/releases/download/1.0.18/libsodium-1.0.18.tar.gz'
 
 mbedtls_file='mbedtls-2.16.6'
 mbedtls_url='https://tls.mbed.org/download/'"$mbedtls_file"'-apache.tgz'
